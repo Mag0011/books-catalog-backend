88ada11a52144c6cd414254ea34ef1342622b694
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2022-03-30 15:56:14 -0600

    Chore: Creation of User and Role entities
    * Entity classes where created for User and Role entities
    * Annotations for ORM-Hibernate were implemented.

---
+++ src/main/java/com/company/books/backend/dao/IUsuarioDao.java
@@ -0,0 +1,14 @@
+package com.company.books.backend.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.company.books.backend.model.User;
+
+public interface IUsuarioDao extends CrudRepository<User, Long>{
+
+	public User findByUserName(String userName);
+	@Query("SELECT u from User u where u.userName = ?1")
+	public User findByUserId();
+	
+}

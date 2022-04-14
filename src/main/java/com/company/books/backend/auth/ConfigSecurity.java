d9b728900e7b9e63f9680165102f441c426a4359
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2022-04-14 13:45:16 -0500

    Feature: Security with Oauth implemented
    * Classes added for enable security:
        AuthorizationServer.java
        ConfigSecurity.java
        ResourceConfiguration.java

--- src/main/java/com/company/books/backend/auth/ConfigSecurity.java
+++ src/main/java/com/company/books/backend/auth/ConfigSecurity.java
@@ -3,8 +3,12 @@ package com.company.books.backend.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,6 +22,15 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean("authenticationManager")
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+
+
 
 	@Override
 	@Autowired
@@ -26,6 +39,15 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 		auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
 	}
 	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().
+		antMatchers(HttpMethod.GET,"/v1/categories").permitAll().
+		anyRequest().authenticated().
+		and().
+		csrf().disable().
+		sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 	
 	
 }
